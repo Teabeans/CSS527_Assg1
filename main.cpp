59fa5f69270a73869ba621426d1394abf173cf73
Teabean <twhlum@gmail.com>
2020-01-29 04:47:45 -0800

    Updates to output

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 #include <string>
 #include <fstream>
 
+#define DEBUG true
+
 int main() {
-  std::cerr << "Hello, World!" << std::endl;
+  if( DEBUG ) {
+  	std::cerr << "Generating an sbox instance..." << std::endl << std::endl;
+  }
   sbox theBox;
-  theBox.renderText();
+  theBox.renderPlaintext( 40 );
 
   // Test Sbox load
   theBox.loadSbox( "S1.txt", "S2.txt" );
@@ -15,13 +19,60 @@ int main() {
   theBox.loadKeys( "keys.txt" );
 
   // Test text load
-  theBox.loadText( "plaintext.txt" );
+  theBox.loadPlaintext( "plaintext.txt" );
 
-  // Test S-box encryption using K1 (rather than K2)
+  theBox.renderPlaintext( 40 );
+
+  // Test S-box encryption using K1
   // Key 1, Row 0
-  theBox.encrypt( "K1", 0 );
+  for( int i = 0 ; i < 10 ; i++ ) {
+    theBox.encrypt( "K1", i );
+  }
 
-  theBox.renderCiphertext( 16 );
+  std::cerr << "Rendering ciphertext from K1..." << std::endl;
+  theBox.renderCiphertext( 40 );
+
+  // Test S-box encryption using K2)
+  // Key 1, Row 0
+  for( int i = 0 ; i < 10 ; i++ ) {
+    theBox.encrypt( "K2", i );
+  }
+
+  std::cerr << "Rendering ciphertext from K2..." << std::endl;
+  theBox.renderCiphertext( 40 );
+
+  // Reset encryption to K1
+  for( int i = 0 ; i < 10 ; i++ ) {
+    theBox.encrypt( "K1", i );
+  }
+
+  std::cerr << "Generating a comparison box..." << std::endl << std::endl;
+
+  sbox compareBox;
+  compareBox.loadSbox( "S1.txt", "S2.txt" );
+  compareBox.loadKeys( "keys.txt" );
+  compareBox.loadPlaintext( "plaintext_1bit.txt" );
+  for( int i = 0 ; i < 10 ; i++ ) {
+    compareBox.encrypt( "K1", i );
+  }
+  compareBox.renderCiphertext( 40 );
+
+  std::cerr << "Converting ComparisonBox messages 0 to 9 to binary string..." << std::endl;
+  std::cerr << compareBox.cipherToString( 0, 9) << std::endl << std::endl;
+
+  std::cerr << "Rendering DefaultBox messages 0 to 9 binary string to blocks..." << std::endl;
+  compareBox.renderBinaryString( theBox.cipherToString(0, 9) );
+  std::cerr << std::endl;
+
+  std::cerr << "Rendering ComparisonBox messages 0 to 9 binary string to blocks..." << std::endl;
+  compareBox.renderBinaryString( compareBox.cipherToString(0, 9) );
+  std::cerr << std::endl;
+
+  std::cerr << "Testing string comparison with 'abcdeg' vs 'abceeg'..." << std::endl;
+  std::cerr << theBox.avalancheCompare( "abcdeg", "abceeg" ) << std::endl << std::endl;
+
+  std::cerr << "Comparing original ciphertext binary string to ComparisonBox binary string..." << std::endl;
+  std::cerr << theBox.avalancheCompare( theBox.cipherToString( 0, 9), compareBox.cipherToString( 0, 9) ) << "\% different." << std::endl;
 
   return 1;
 }
--- sbox.h
+++ sbox.h
@@ -13,9 +13,12 @@ class sbox {
   unsigned char S1Linear[16];
   unsigned char S2[4][4]; // [row][col]
   unsigned char S2Linear[16];
+
+  // Keys
   unsigned char K1[4];
   unsigned char K2[4];
 
+  // Plaintext and ciphertext buffers
   unsigned char plaintext[ MAX_MSG_LENGTH ];
   unsigned char ciphertext[ MAX_MSG_LENGTH ];
 
@@ -26,16 +29,28 @@ class sbox {
   void tareFields();
 
   // Loaders
-  bool loadText( std::string filename );
+  bool loadPlaintext( std::string filename );
   bool loadSbox( std::string filenameS1, std::string filenameS2 );
   bool loadKeys( std::string filename );
 
+  // Workers
   void encrypt( std::string keyDesignate, int row );
+  double avalancheCompare( std::string seq1, std::string seq2 );
 
-  // Render the text
-  void renderText( );
-  void renderCiphertext( int length );
+  // Convert data from one to another
   int convertBinaryToInt( std::string binaryString );
   char convertIntToChar( int input );
+
+  // Convert a row selection to string
+  std::string cipherToString( int min, int max );
+
+  // Render texts
+  void renderBinaryString( std::string bitSequence );
+  void renderPlaintext( int length );
+  void renderCiphertext( int length );
+
+
+  // Getters/Setters
+  // None for this class
 };
 
